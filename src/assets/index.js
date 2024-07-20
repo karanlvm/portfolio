62268e9c8e5ff66c997219448870e6255e0b7989
Karan Vasudevamurthy <karanlvm123@gmail.com>
2024-07-20 13:31:32 -0500

    New portfolio

---
+++ src/assets/index.js
@@ -0,0 +1,10 @@
+import close from "./close.svg";
+import logo from "./logo.svg";
+import menu from "./menu.svg";
+
+import ham from "./ham.jpg";
+import telegram from "./telegram.png";
+import musiqi from "./musiqi.jpg";
+
+export { ham, close, telegram, logo, menu, musiqi };
+
